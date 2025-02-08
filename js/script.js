f78d983a0f6538b0041f2a118a1461aa700f6a44
ezveducastur <szp05514@educastur.es>
2025-02-08 17:16:11 +0100

    Corregido problema en script.js

--- js/script.js
+++ js/script.js
@@ -22,5 +22,10 @@ document.addEventListener("DOMContentLoaded", function() {
         if (!valid) {
             event.preventDefault();
         }
+
+        if (mensaje.value.trim() === "") {
+            alert("Por favor, ingrese un mensaje.");
+            valid = false;
+        }
     });
 });
